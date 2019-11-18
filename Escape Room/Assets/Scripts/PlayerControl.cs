320da7ad58065a5dcf74e270a87430a77e8a0276
Oli <oli.taylor.2001@gmail.com>
2019-11-18 12:07:13 +0000

    Basic Movement
    Player can move forward and backwards, as well as strafe left to right.

---
+++ Escape Room/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
+    CharacterController charController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 direction = transform.TransformDirection(Vector3.forward);
+            charController.SimpleMove(direction * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 direction = transform.TransformDirection(Vector3.back);
+            charController.SimpleMove(direction * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 direction = transform.TransformDirection(Vector3.left);
+            charController.SimpleMove(direction * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 direction = transform.TransformDirection(Vector3.right);
+            charController.SimpleMove(direction * moveSpeed);
+        }
+
+    }
+}
