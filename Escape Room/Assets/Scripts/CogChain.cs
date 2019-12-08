71439012c638847403bfb4949a317767c76b9df6
Oli <oli.taylor.2001@gmail.com>
2019-12-08 17:02:28 +0000

    First Puzzle Complete
    The player can now pick up the cog and place it in the cog chain to finish the chain.
    
    currently this achieves nothing, and models of the cogs need to be added, as well as animations for the cogs.

---
+++ Escape Room/Assets/Scripts/CogChain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogChain : MonoBehaviour
+{
+    public GameObject cog;
+    public bool complete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        complete = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        if (cog.GetComponent<Cog>().inHands == true)
+        {
+            complete = true;
+        }
+    }
+}
---
+++ Escape Room/Assets/Scripts/Interactables/Cog.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cog : MonoBehaviour
+{
+
+    public bool inHands;
+    public Transform camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inHands = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(camera.position, transform.position) > 0.1 && inHands == true)
+        {
+            Vector3 target = camera.position;
+            transform.LookAt(camera);
+            transform.position = Vector3.MoveTowards(transform.position, target, (1f * Time.deltaTime));
+        }
+    }
+
+    public IEnumerator OnMouseDown()
+    {
+        inHands = true;
+        yield return new WaitForSeconds(1.5f);
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
