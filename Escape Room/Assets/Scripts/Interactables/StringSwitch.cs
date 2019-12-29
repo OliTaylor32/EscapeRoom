029d275f4435e8ec80912c12a2ff21e931b56f01
Oli <oli.taylor.2001@gmail.com>
2019-12-29 01:04:53 +0000

    StringSwitch Functionality
    Waterfall, waterwheel and water only starts animating once the string switch is pulled.

---
+++ Escape Room/Assets/Scripts/Interactables/StringSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringSwitch : MonoBehaviour
+{
+    private bool active;
+    public GameObject waterFall;
+    public GameObject waterWheel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        print("Clicked");
+        gameObject.GetComponent<Animator>().Play("StringSwitchPull");
+
+        if (active == false)
+        {
+            active = true;
+            waterFall.GetComponent<WaterFall>().Activate();
+        }
+        gameObject.GetComponent<Animator>().Play("StringSwitchIdle");
+
+    }
+}
--- Escape Room/Assets/Scripts/WaterFall.cs
+++ Escape Room/Assets/Scripts/WaterFall.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class WaterFall : MonoBehaviour
 {
+    private bool active;
+    public GameObject water;
+    public GameObject wheel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, -1000 * Time.deltaTime, 0);
+        if (active == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 4, 1), 1 * Time.deltaTime);
+            wheel.transform.Rotate(0, 30 * Time.deltaTime, 0);
+        }
+
+    }
+
+    public void Activate()
+    {
+        active = true;
+        water.GetComponent<Animator>().Play("WaterRise");
+        water.GetComponent<Animator>().Play("WaterStart");
     }
 }
